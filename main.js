9d998fde494642773eb6f0eec33d0f4179af1a94
Hamadi Zakarya <zakarya.code@gmail.com>
2025-12-07 19:30:32 +0100

    gigi

---
+++ main.js
@@ -0,0 +1,25 @@
+// CODE GSAP SCROLL HOME PIN
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.create({
+  trigger: ".home",
+  start: "top top",
+  end: "+=100%",
+  pin: true,
+  pinSpacing: false,
+});
+// CODE GSAP SCROLL STORE VERTICAL
+const container = document.querySelector(".horizontal-container");
+const boxes = gsap.utils.toArray(".box");
+
+gsap.to(container, {
+  x: () => -(container.scrollWidth - window.innerWidth) + "px",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".store",
+    start: "top top",
+    end: () => "+=" + container.scrollWidth + "px",
+    scrub: true,
+    pin: true, // تثبيت القسم أثناء التمرير
+    anticipatePin: 1,
+  },
+});
